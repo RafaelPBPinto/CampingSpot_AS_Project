4d73c90055aba17a0a50f7988485dd76eefe470e
Rafael <rafaelpbpinto2@gmail.com>
2022-06-03 15:58:11 +0100

    Estrutura inicial para o Django & HTML do proto.io

---
+++ mysite/polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Parque(models.Model):
+    parque_name = models.CharField('name')
+    parque_descricao = models.CharField('description')
+    parque_rating = models.IntegerField(default=0)
+
+class User(models.Model):
+    user_name = models.CharField('username')
+    user_history = models ## acabar
+
+class Reserva(models.Model):
+    reserva_user = models.ForeignKey(User)
+    reserva_price = models.IntegerField(default=0)
+    reserva_atividades = models.CharField('atividade')
+    reserva_npessoas = models.IntegerField(default=1)
+    reserva_data = models ## acabar
+    
