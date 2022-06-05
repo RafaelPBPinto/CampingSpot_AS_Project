173143248c87694f6a4f7f537f08edb00ee6bf92
Rafael <rafaelpbpinto2@gmail.com>
2022-06-05 20:30:58 +0100

    site

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'polls/index.html')
+
+def about(request):
+    return render(request, 'polls/about.html')
