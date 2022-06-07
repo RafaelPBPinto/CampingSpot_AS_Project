52fbe101debc7a6e7335d901a03d4b1e2c76493f
zpdomingues16 <jdomingues@ua.pt>
2022-06-07 23:02:28 +0100

    db

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,19 +1,16 @@
 from django.db import models
 
-"""
-class Parque(models.Model):
-    parque_name = models.CharField('name')
-    parque_descricao = models.CharField('description')
-    parque_rating = models.IntegerField(default=0)
+class Atividades(models.Model):
+    atvs = models.TextField()
 
-class User(models.Model):
-    user_name = models.CharField('username')
-    user_history = models ## acabar
+class Parques(models.Model): 
+    nome = models.CharField(max_length = 100)
+    location = models.CharField(max_length = 100)
+    activities = models.ForeignKey(Atividades, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    pricepn = models.IntegerField()
+    nreservas = models.IntegerField()
+    nvagas = models.IntegerField()
+    image = models.ImageField(default='default.jpg')
 
-class Reserva(models.Model):
-    reserva_user = models.ForeignKey(User)
-    reserva_price = models.IntegerField(default=0)
-    reserva_atividades = models.CharField('atividade')
-    reserva_npessoas = models.IntegerField(default=1)
-    reserva_data = models ## acabar
-"""    
+
