536677f4e9c73920f88e80271adbee4af5e5bf6e
zpdomingues16 <jdomingues@ua.pt>
2022-06-07 23:35:11 +0100

    dba

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-class Atividades(models.Model):
-    atvs = models.TextField()
+
+
+
 
 class Parques(models.Model): 
     nome = models.CharField(max_length = 100)
     location = models.CharField(max_length = 100)
-    activities = models.ForeignKey(Atividades, on_delete=models.CASCADE)
+    activities =ArrayField(ArrayField(models.CharField(max_length = 100)))
     rating = models.IntegerField()
     pricepn = models.IntegerField()
     nreservas = models.IntegerField()
     nvagas = models.IntegerField()
     image = models.ImageField(default='default.jpg')
-
 
