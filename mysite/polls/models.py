3b648effb1396fa454d9ffcd292244acef4aa6cf
Rafael <rafaelpbpinto2@gmail.com>
2022-06-08 01:37:36 +0100

    atualizar html

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,11 +1,8 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
-
-
-
-
-class Parques(models.Model): 
+class Parque(models.Model): 
     nome = models.CharField(max_length = 100)
     location = models.CharField(max_length = 100)
     activities =ArrayField(ArrayField(models.CharField(max_length = 100)))
@@ -13,5 +10,5 @@ class Parques(models.Model):
     pricepn = models.IntegerField()
     nreservas = models.IntegerField()
     nvagas = models.IntegerField()
-    image = models.ImageField(default='default.jpg')
+    image = models.ImageField(default='default.jpg', upload_to='parques_pics')
 
