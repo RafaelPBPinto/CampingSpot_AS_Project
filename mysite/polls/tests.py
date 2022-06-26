59c8e71f4d81e4b07de12f5086c11305bfa894b3
Tiago <tiago.r.santos@ua.pt>
2022-06-26 14:53:58 +0100

    update

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase, TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 # Create your tests here.
+
+class PlayerFromTest(LiveServerTestCase):
+
+    def testSearch(self):
+        selenium = webdriver.Chrome()
+
+        selenium.get('https://campingspot.herokuapp.com/')
+
+        search = selenium.find_element(By.ID,"searched")
+
+        submit = selenium.find_element(By.ID,"submit")
+
+        search.send_keys('Madeira')
+
+        submit.send_keys(Keys.RETURN)
+
+        assert 'Madeira' in selenium.page_source
+
